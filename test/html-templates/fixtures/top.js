051e2985517b6ca126d79b96390f5153780a0186
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-04-23 09:17:41 +0200

    Working on a fix for the refactor branch

---
+++ test/html-templates/fixtures/top.js
@@ -0,0 +1,27 @@
+(function(module) {
+try {
+  module = angular.module('templates');
+} catch (e) {
+  module = angular.module('templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('_app/a.ngt',
+    '<h1>A</h1>\n' +
+    '');
+}]);
+})();
+
+
+
+(function(module) {
+try {
+  module = angular.module('templates');
+} catch (e) {
+  module = angular.module('templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('_app/b.ngt',
+    '<h1>A</h1>\n' +
+    '');
+}]);
+})();
