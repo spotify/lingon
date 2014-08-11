c3f33954ea47549d6f4b57dd424ce6be32a6be15
Philip von Bargen <pvb@spotify.com>
2014-08-11 16:15:28 +0200

    Rename old references to OJ

--- test/processors/lingon.js
+++ test/processors/lingon.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var es = require('event-stream');
-var oj = require('../../lib/boot');
+var lingon = require('../../lib/boot');
 
 var createProcessor = function(type) {
   return es.map(function(file, cb) {
@@ -15,66 +15,66 @@ var createProcessor = function(type) {
 };
 
 // pre processors
-oj.preProcessor('simplesyntax', function(params) {
+lingon.preProcessor('simplesyntax', function(params) {
   return createProcessor('simple preprocessor that will be overwritten ');
 });
-oj.preProcessor('simplesyntax', function(params) {
+lingon.preProcessor('simplesyntax', function(params) {
   return createProcessor('simple pre');
 });
 
-oj.preProcessor('alternativesyntax', function(params) {
+lingon.preProcessor('alternativesyntax', function(params) {
   return [
     createProcessor('alternative pre')
   ];
 });
 
-oj.preProcessor('multiplesyntax', function(params) {
+lingon.preProcessor('multiplesyntax', function(params) {
   return [
     createProcessor('multiple1 pre'),
     createProcessor('multiple2 pre')
   ];
 });
 
-oj.preProcessor('orderedsyntax', function(params) {
+lingon.preProcessor('orderedsyntax', function(params) {
   return createProcessor('ordered pre');
 });
 
 
 
 // post processors
-oj.postProcessor('simplesyntax').set(function(params) {
+lingon.postProcessor('simplesyntax').set(function(params) {
   return createProcessor('simple postprocessor that will be overwritten ');
 });
-oj.postProcessor('simplesyntax').set(function(params) {
+lingon.postProcessor('simplesyntax').set(function(params) {
   return createProcessor('simple post');
 });
-oj.postProcessor('simplesyntax').add(/matching/, function(params) {
+lingon.postProcessor('simplesyntax').add(/matching/, function(params) {
   return createProcessor('simple path-matching post');
 });
 
-oj.postProcessor('alternativesyntax').add(function(params) {
+lingon.postProcessor('alternativesyntax').add(function(params) {
   return [
     createProcessor('alternative post')
   ];
 });
 
-oj.postProcessor('multiplesyntax').add(function(params) {
+lingon.postProcessor('multiplesyntax').add(function(params) {
   return createProcessor('multiple1 post');
 });
-oj.postProcessor('multiplesyntax').add(function(params) {
+lingon.postProcessor('multiplesyntax').add(function(params) {
   return [
     createProcessor('multiple2 post'),
     createProcessor('multiple3 post')
   ];
 });
 
-oj.postProcessor('orderedsyntax').push(function(params) {
+lingon.postProcessor('orderedsyntax').push(function(params) {
   return createProcessor('ordered1 post');
 });
-oj.postProcessor('orderedsyntax').unshift(function(params) {
+lingon.postProcessor('orderedsyntax').unshift(function(params) {
   return createProcessor('ordered2 post');
 });
-oj.postProcessor('orderedsyntax').add(function(params) {
+lingon.postProcessor('orderedsyntax').add(function(params) {
   return [
     createProcessor('ordered3 post'),
     createProcessor('ordered4 post')
