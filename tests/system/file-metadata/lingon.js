67aa52d8c1373a7a87a247a397572f4340a4c49e
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-09-01 23:27:32 +0200

    Updated file-metadata and global system tests to use the context and global objects instead of accessing properties directly.

--- tests/system/file-metadata/lingon.js
+++ tests/system/file-metadata/lingon.js
@@ -6,15 +6,15 @@ var spawn  = require('child_process').spawn;
 
 lingon.preProcessors.unshift('ejs', function(global, context) {
   return es.map(function(file, cb) {
-      var shasum = spawn('shasum', [
+      var ls = spawn('ls', [
         context.file
       ]);
 
-      shasum.stdout.on('data', function (data) {
+      ls.stdout.on('data', function (data) {
         context.metadata = data.toString().trim();
       });
 
-      shasum.on('close', function (data) {
+      ls.on('close', function (data) {
         cb(null, file);
       });
     });
