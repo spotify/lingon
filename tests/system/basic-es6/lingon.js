1d34777b911ef1045d8cea49c2920fb5743cb737
Philip von Bargen <pvb@spotify.com>
2015-12-22 11:25:21 -0500

    Make babel work (also with template files)

--- tests/system/basic-es6/lingon.js
+++ tests/system/basic-es6/lingon.js
@@ -2,28 +2,42 @@
 
 'use strict';
 
+var path = require('path');
+var LingonClass = require('../../../lib/lingon');
 var lingon = require('../../../lib/boot');
 var Builder = require('systemjs-builder');
 var es = require('event-stream');
 
-lingon.preProcessors.push('js', function(params) {
-  var builder = new Builder(lingon.config.rootPath, {
-    defaultJSExtensions : true,
+var builder = new Builder(lingon.rootPath, {
+  defaultJSExtensions : true,
 
-    // opt in to Babel for transpiling over Traceur
-    transpiler          : 'babel'
+  // opt in to Babel for transpiling over Traceur
+  transpiler          : 'babel',
+});
+
+lingon.postProcessors.push('js', function() {
+  return es.map(function(file, cb) {
+    var filePath = path.join(lingon.config.sourcePath,
+        path.relative(file.base, file.path));
+
+    builder.buildStatic(filePath, {runtime: true})
+      .then(function(output) {
+        file.contents = new Buffer(output.source);
+        cb(null, file);
+      })
+      .catch(function(err) {
+        cb(err);
+      });
   });
+});
+
+lingon.postProcessors.push(['js', 'css', 'less'], function() {
+  let year = (new Date()).getFullYear();
 
   return es.map(function(file, cb) {
-    builder.buildSFX(
-      params.context.file, {runtime: false}
-    )
-    .then(function(output) {
-      file.contents = new Buffer(output.source);
-      cb(null, file);
-    })
-    .catch(function(err) {
-      cb(err);
-    });
+    let header = '/*!\n* Copyright (c) ' + year + ' Spotify AB\n*/\n';
+    file.contents = new Buffer(header + file.contents.toString());
+
+    cb(null, file);
   });
 });
--- tests/system/basic-es6/source/index.js
+++ tests/system/basic-es6/source/index.js
@@ -3,6 +3,8 @@
 import Util from './_lib/lib';
 import {thirdPartyString} from './_vendor/thirdParty1';
 import * as thirdParty2 from './_vendor/thirdParty2';
+import template from './_lib/template.html!text';
+
 
 function hello() {
   var util = new Util();
@@ -10,6 +12,7 @@ function hello() {
 
   console.log(thirdPartyString);
   console.log(thirdParty2.anotherThirdPartyString);
+  console.log(template);
 }
 
 hello();
